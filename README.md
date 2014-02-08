179074ac72bfc9c53bd5a415b0a7337736255d41
Diego Moreira <diegoalvesmoreira@gmail.com>
2014-02-08 19:28:05 -0200

    Implemented the Slack after step integration

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
-step-slack-notify
-=================
+# step-slack-notify
 
 Send a message to a Slack channel after build or deploy
+
+## Options
+
+### required
+
+> Set the `webhook-url` as property of the step or as deploy target or application environment variable
+
+- `webhook-url` - Slack webhook integration url.
+
+### optional
+
+- `passed-message` - Use this option to override the default passed message
+- `failed-message` - Use this option to override the default failed message
+
+## Example
+
+```yml
+build:
+	after-steps:
+		- slack-notify:
+			webhook-url: "PUT YOUR SLACK WEBHOOK URL HERE"
+```
---
+++ run.sh
@@ -0,0 +1,30 @@
+
+if [ ! -n "$WERCKER_SLACK_WEBHOOK_URL" ]; then
+	error 'Please specify the Slack team webhook url and activate the integration on your Slack settings'
+	exit 1
+fi
+
+if [ ! -n "$WERCKER_SLACK_NOTIFY_PASSED_MESSAGE" ]; then
+	if [ ! -n "$DEPLOY"]; then
+		export WERCKER_SLACK_NOTIFY_PASSED_MESSAGE="$WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: build of $WERCKER_GIT_BRANCH by $WERCKER_STARTED_BY passed."
+	else
+		export WERCKER_SLACK_NOTIFY_PASSED_MESSAGE="$WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: deploy to $WERCKER_DEPLOYTARGET_NAME by $WERCKER_STARTED_BY passed."
+	fi
+fi
+
+if [ ! -n "$WERCKER_SLACK_NOTIFY_FAILED_MESSAGE" ]; then
+	if [ ! -n "$DEPLOY"]; then
+		export WERCKER_SLACK_NOTIFY_FAILED_MESSAGE="$WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: build of $WERCKER_GIT_BRANCH by $WERCKER_STARTED_BY failed."
+	else
+		export WERCKER_SLACK_NOTIFY_FAILED_MESSAGE="$WERCKER_APPLICATION_OWNER_NAME/$WERCKER_APPLICATION_NAME: deploy to $WERCKER_DEPLOYTARGET_NAME by $WERCKER_STARTED_BY failed."
+	fi
+fi
+
+if [ ! -n "$WERCKER_RESULT" = "passed" ]; then
+	export WERCKER_SLACK_NOTIFY_MESSAGE="$WERCKER_SLACK_NOTIFY_PASSED_MESSAGE"
+	export ICON_EMOJI=":white_check_mark:"
+else
+	export WERCKER_SLACK_NOTIFY_MESSAGE="$WERCKER_SLACK_NOTIFY_FAILED_MESSAGE"
+fi
+
+curl -X POST -H "Content-Type: application/json" -d "payload={ \"channel\": \"#equalizze-api\", \"username\": \"wercker\", \"text\": \"$WERCKER_SLACK_NOTIFY_MESSAGE\", \"icon_emoji\": \"$ICON_EMOJI\" }" $WERCKER_SLACK_WEBHOOK_URL
